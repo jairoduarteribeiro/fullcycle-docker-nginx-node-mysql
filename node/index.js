fb966c7706bdba18f6378e76b8e03d1556001aa4
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2024-10-27 16:12:30 +0000

    Insert name

--- node/index.js
+++ node/index.js
@@ -1,13 +1,47 @@
 const express = require("express");
-const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+const { faker, th } = require("@faker-js/faker");
+
+const connection = mysql.createConnection({
+  host: "db",
+  user: "root",
+  password: "secret",
+  database: "fullcycle",
+});
+
+connection.connect();
 
 const app = express();
 const port = 3000;
 
+function insertName(name) {
+  const sql = `INSERT INTO people (name) VALUES ('${name}')`;
+  connection.query(sql, (error, results, fields) => {
+    if (error) {
+      throw error;
+    }
+  });
+}
+
 app.get("/", (req, res) => {
   const name = faker.person.fullName();
-  res.send(`Hello ${name}`);
+  try {
+    insertName(name);
+    res.send("Full Cycle Rocks!");
+  } catch (error) {
+    res
+      .status(500)
+      .send("An error occurred while inserting the name into the database.");
+  }
 });
+
+["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) =>
+  process.on(signal, () => {
+    connection.end();
+    console.log(`Connection closed`);
+    process.exit();
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
